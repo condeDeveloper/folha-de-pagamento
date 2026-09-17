daded0156c1e4a82813514377bd3fcac54d0f987
condeDeveloper <183445812+condeDeveloper@users.noreply.github.com>
2026-09-17 10:31:42 -0300

    Usings globais dos testes

---
+++ tests/Folha.Tests/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using FluentAssertions;
+global using Folha.Core.Cadastro;
+global using Folha.Core.Calculo;
+global using Folha.Core.Comum;
+global using Folha.Core.Relatorio;
+global using Folha.Core.Rubricas;
+global using Folha.Core.Tabelas;
+global using Xunit;
